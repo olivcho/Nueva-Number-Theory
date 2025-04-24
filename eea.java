364a6fdff9587ac5a484510fe62cdf3c731bc980
olivcho <olivercho007@gmail.com>
2025-04-24 01:13:47 -0700

    add extended euclidean algorithm calculator

---
+++ eea.java
@@ -0,0 +1,11 @@
+public class eea {
+	public static void main(String[] args) {
+		int a = Integer.parseInt(Math.max(args[0], args[1]));
+		int b = Integer.parseInt(Math.min(args[0], args[1]));
+		int r = a % b;
+
+		while (r != 0) {
+				
+		}	
+	}
+}
