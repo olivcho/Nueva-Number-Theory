010b6897e431398556f47f2085e55c9dac724d21
olivcho <olivercho007@gmail.com>
2025-04-24 11:01:28 -0700

    implement euclidean algo

--- eea.java
+++ eea.java
@@ -1,11 +1,30 @@
 public class eea {
 	public static void main(String[] args) {
-		int a = Integer.parseInt(Math.max(args[0], args[1]));
-		int b = Integer.parseInt(Math.min(args[0], args[1]));
+		int a = Integer.parseInt(args[0]);
+		int b = Integer.parseInt(args[1]);
+		int q = a / b;
 		int r = a % b;
 
+		// make sure a is greater than b
+		if (a < b) {
+			int temp = a;
+			a = b;
+			b = temp;
+		}
+
+		// calculation loop
 		while (r != 0) {
-				
-		}	
+			q = a / b;
+			r = a % b;
+
+			System.out.println(a + " = (" + q + ")(" + b + ") + " + r);			
+
+			if (r != 0) {
+				a = b;
+				b = r;
+			}
+
+		}
+		System.out.println("GCD: " + b);
 	}
 }
